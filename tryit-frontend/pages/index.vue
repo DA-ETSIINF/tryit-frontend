79bd8c88f56d606624055f50efc1df4108802000
onmax <maximogarciamtnez@gmail.com>
2020-02-12 23:02:09 +0100

    added basic template and resources interfaces

---
+++ tryit-frontend/pages/index.vue
@@ -0,0 +1,10 @@
+<template></template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+@Component({})
+export default class extends Vue {}
+</script>
+
+<style scoped>
+</style>
---
+++ tryit-frontend/router/routes.ts
@@ -0,0 +1,8 @@
+import { RouteConfig } from "vue-router"
+
+export const routes: RouteConfig[] = [
+	{
+		path: "/",
+		name: "Home"
+	}
+]
