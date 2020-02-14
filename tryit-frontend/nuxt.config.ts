e990d07d847468634b9c5c6f13b52b0841006633
onmax <maximogarciamtnez@gmail.com>
2020-02-14 12:22:18 +0100

    added css variables and exports components

--- tryit-frontend/nuxt.config.ts
+++ tryit-frontend/nuxt.config.ts
@@ -7,7 +7,22 @@ export default {
 			{ name: "viewport", content: "width=device-width, initial-scale=1" },
 			{ hid: "description", name: "description", content: "Official website for Try IT! congress" }
 		],
-		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+		link: [
+			{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+			{
+				rel: "stylesheet",
+				href:
+					"https://fonts.googleapis.com/css?family=Roboto:400,500,700|Source+Sans+Pro:400,600&display=swap"
+			},
+			{
+				rel: "stylesheet",
+				href: "https://unpkg.com/swiper/css/swiper.min.css"
+			}
+		],
+		script: [
+			{ src: "https://kit.fontawesome.com/358fa893fd.js", body: true },
+			{ src: "https://unpkg.com/swiper/js/swiper.min.js", body: true }
+		]
 	},
 	loading: { color: "#3B8070" },
 	css: ["~/assets/css/main.css"],
