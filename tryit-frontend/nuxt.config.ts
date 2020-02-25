fb0393590be7e6d63ce51e5da38211b44a7ed14b
onmax <maximogarciamtnez@gmail.com>
2020-02-25 11:38:09 +0100

    added info for SEO

--- tryit-frontend/nuxt.config.ts
+++ tryit-frontend/nuxt.config.ts
@@ -1,14 +1,79 @@
 export default {
 	env: {},
 	head: {
-		title: "tryit-frontend",
+		title: "Try IT!",
 		meta: [
 			{ charset: "utf-8" },
 			{ name: "viewport", content: "width=device-width, initial-scale=1" },
-			{ hid: "description", name: "description", content: "Official website for Try IT! congress" }
+			{
+				hid: "description",
+				name: "description",
+				content:
+					"Página oficial del Congreso TryIt. Organizado por Delegación de Alumnos de la ETSIINF. Este año se celebra la edición 2020. Charlas, talleres, actividades... Y mucho más. Además, es una actividad acreditable para alumnos de la ETSIINF"
+			},
+			{
+				hid: "geo.position",
+				name: "geo.position",
+				content: "40°24'16.9'' N 3°50'20.8''W"
+			},
+			{
+				hid: "geo.placename",
+				name: "geo.placename",
+				content: "Bloque 1 - Campus Montegancedo de la UPM"
+			},
+			{
+				hid: "geo.region",
+				name: "geo.region",
+				content: "es"
+			},
+			{
+				hid: "robots",
+				name: "robots",
+				content: "index"
+			},
+			{
+				hid: "theme-color",
+				name: "theme-color",
+				content: "#035b9e"
+			},
+			{
+				hid: "keywords",
+				name: "keywords",
+				content:
+					"TryIt, tryit, patrocina, tecnología, conferencia, informática, hackathon, nuevas tecnologias, conference, technology, university, universidad, upm, politecnica, madrid, etsiinf, fi"
+			},
+			{
+				hid: "DC.title",
+				name: "DC.title",
+				lang: "es",
+				content: "Try IT!"
+			},
+			{
+				hid: "DC.creator",
+				name: "DC.creator",
+				content: "Delegación de Alumnos ETSIINF"
+			},
+			{
+				hid: "DC.description",
+				name: "DC.description",
+				lang: "es",
+				content:
+					"Página oficial del Congreso TryIt. Organizado por Delegación de Alumnos de la ETSIINF. Este año se celebra la edición 2020. Charlas, talleres, actividades... Y mucho más. Además, es una actividad acreditable para alumnos de la ETSIINF"
+			},
+			{
+				hid: "DC.language",
+				name: "DC.language",
+				scheme: "RFC1766",
+				content: "es"
+			},
+			{
+				hid: "DC.coverage",
+				name: "DC.coverage",
+				content: "Spain"
+			}
 		],
 		link: [
-			{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+			{ rel: "shortcut icon", type: "image/png", href: "/logo.png" },
 			{
 				rel: "stylesheet",
 				href:
@@ -21,10 +86,11 @@ export default {
 		],
 		script: [
 			{ src: "https://kit.fontawesome.com/358fa893fd.js", body: true },
-			{ src: "https://unpkg.com/swiper/js/swiper.min.js", body: true }
+			{ src: "https://unpkg.com/swiper/js/swiper.min.js", body: true },
+			{ scr: "https://www.googletagmanager.com/gtag/js?id=UA-134663969-1", async: true }
 		]
 	},
-	loading: { color: "#3B8070" },
+	loading: { color: "#035b9e" },
 	css: ["~/assets/css/main.css", "~/assets/css/fullpage.css"],
 	build: {},
 	buildModules: ["@nuxt/typescript-build"],
