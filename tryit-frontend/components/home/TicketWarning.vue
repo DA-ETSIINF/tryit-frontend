4f97deb3895e53756f2986fd9286079e7c16537c
Patala2004 <christian.most@hotmail.com>
2024-03-11 02:35:34 +0100

    test

--- tryit-frontend/components/home/TicketWarning.vue
+++ tryit-frontend/components/home/TicketWarning.vue
@@ -17,7 +17,17 @@ export default {
     },
     async fetch() {
         const response = this.$axios.$get(process.env.api + '/api/tickets')
-        
+        if (process.client) {
+            console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAa')
+            console.log(process.env.api)
+            console.log(response)
+            this.tickets = response
+            for (const ticket in response) {
+                console.log(ticket)
+            }
+            console.log('bBBBBBBBBBBBBBBB')
+        }
+        response = this.$axios.$get(process.env.api + '/api/auth')
         if (process.client) {
             console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAa')
             console.log(process.env.api)
