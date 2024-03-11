7f0563927e1242dd747da99cdcd824ff92acfa35
dmunozv04 <39565245+dmunozv04@users.noreply.github.com>
2024-03-11 09:48:02 +0100

    fix snackbar not appearing

--- tryit-frontend/components/home/TicketWarning.vue
+++ tryit-frontend/components/home/TicketWarning.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-snackbar v-model="snackbar" :color="snackbarColor" top>
+    <v-snackbar v-model="snackbar" :color="snackbarColor" top :timeout="5000">
         {{ snackbarMessage }}
         <v-btn text @click="snackbar = false">Close</v-btn>
     </v-snackbar>
@@ -12,13 +12,12 @@ export default {
             snackbar: false,
             snackbarMessage: 'Has iniciado sesión pero todavía no has sacado entrada. ¡Saca tu entrada en la sección de entradas para acreditar las charlas!',
             snackbarColor: 'warning', // or 'error', 'warning', 'info'
-            tickets: [],
         };
     },
-    async fetch() {
-        const response = this.$axios.$get(process.env.api + '/api/tickets')
+    created() {
+
         if(this.$nuxt.$auth.loggedIn && this.$nuxt.$auth.user.ticket_id === "undefined"){
-            snackbar = true
+            this.snackbar = true
         }
     },
 };
