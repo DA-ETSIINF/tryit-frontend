2dbed89510a603d5feb6a3a1b0ed6fe42e131a8e
afpipe <viloria.hernandez.af@gmail.com>
2025-03-07 14:51:34 +0100

    Fotos de las rutas de buses actualizadas

--- tryit-frontend/pages/index.vue
+++ tryit-frontend/pages/index.vue
@@ -188,27 +188,27 @@
 		<v-container style="max-width:392px; max-height:392px; padding:0; margin-top:30px">
 			<v-carousel cycle hide-delimiters show-arrows>
 				<v-carousel-item>
-					<img src="img/map_slider/como_llegar_1.png"
+					<img src="img/map_slider/como_llegar_865.jpeg"
 						style="max-height:100%; max-width:100% ; object-fit: contain;">
 				</v-carousel-item>
 				<v-carousel-item>
-					<img src="img/map_slider/como_llegar_2.png"
+					<img src="img/map_slider/como_llegar_591.jpeg"
 						style="max-height:100%; max-width:100% ; object-fit: contain;">
 				</v-carousel-item>
 				<v-carousel-item>
-					<img src="img/map_slider/como_llegar_3.png"
+					<img src="img/map_slider/como_llegar_566.jpeg"
 						style="max-height:100%; max-width:100% ; object-fit: contain;">
 				</v-carousel-item>
 				<v-carousel-item>
-					<img src="img/map_slider/como_llegar_4.png"
+					<img src="img/map_slider/como_llegar_571.jpeg"
 						style="max-height:100%; max-width:100% ; object-fit: contain;">
 				</v-carousel-item>
 				<v-carousel-item>
-					<img src="img/map_slider/como_llegar_5.png"
+					<img src="img/map_slider/como_llegar_573.jpeg"
 						style="max-height:100%; max-width:100% ; object-fit: contain;">
 				</v-carousel-item>
 				<v-carousel-item>
-					<img src="img/map_slider/como_llegar_6.png"
+					<img src="img/map_slider/como_llegar_ml3.jpeg"
 						style="max-height:100%; max-width:100% ; object-fit: contain;">
 				</v-carousel-item>
 			</v-carousel>
