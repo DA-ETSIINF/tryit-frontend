0468bc23d1a134858c17faf649d969aae0536455
Felix Garcia Taboada <kali@kali>
2025-03-12 12:20:34 -0400

    Movido el slider de buses a su propio componente

---
+++ tryit-frontend/components/home/BusSlider.vue
@@ -0,0 +1,30 @@
+<template>
+<v-container style="max-width:392px; max-height:392px; padding:0; margin-top:30px">
+			<v-carousel cycle hide-delimiters show-arrows>
+				<v-carousel-item>
+					<img src="img/map_slider/como_llegar_865.jpeg"
+						style="max-height:100%; max-width:100% ; object-fit: contain;">
+				</v-carousel-item>
+				<v-carousel-item>
+					<img src="img/map_slider/como_llegar_591.jpeg"
+						style="max-height:100%; max-width:100% ; object-fit: contain;">
+				</v-carousel-item>
+				<v-carousel-item>
+					<img src="img/map_slider/como_llegar_566.jpeg"
+						style="max-height:100%; max-width:100% ; object-fit: contain;">
+				</v-carousel-item>
+				<v-carousel-item>
+					<img src="img/map_slider/como_llegar_571.jpeg"
+						style="max-height:100%; max-width:100% ; object-fit: contain;">
+				</v-carousel-item>
+				<v-carousel-item>
+					<img src="img/map_slider/como_llegar_573.jpeg"
+						style="max-height:100%; max-width:100% ; object-fit: contain;">
+				</v-carousel-item>
+				<v-carousel-item>
+					<img src="img/map_slider/como_llegar_ml3.jpeg"
+						style="max-height:100%; max-width:100% ; object-fit: contain;">
+				</v-carousel-item>
+			</v-carousel>
+		</v-container>
+</template>
