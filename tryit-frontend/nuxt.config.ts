3e81c71d240e4fd159a119d5bfd8bafe46312cf7
Chengjian <chengjianzhou@vitus.com>
2025-03-12 15:36:09 +0100

    Prueba 4: soy malisimo en esto

--- tryit-frontend/nuxt.config.ts
+++ tryit-frontend/nuxt.config.ts
@@ -143,8 +143,8 @@ export default {
 			})
 
 			routes.push({
-				name: "email_verification",
-				path: "pages/email_verification.vue",
+				name: "email-verification",
+				path: "pages/email-verification.vue",
 				component: resolve(__dirname, "pages/email_verification.vue")
 			})
 		}
