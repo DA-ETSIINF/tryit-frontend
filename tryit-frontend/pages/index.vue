7bda338c79f12c90398e36f7363ea27e18ebec18
Chengjian <chengjianzhou@vitus.com>
2026-02-10 12:32:58 +0100

    añadido busqueda de voluntarios

--- tryit-frontend/pages/index.vue
+++ tryit-frontend/pages/index.vue
@@ -34,11 +34,11 @@
 		<hr>
 
 		<!-- PONER DE VUELTA EL AÑO QUE VIENE SI SE BUSACAN VOLUNTARIOS -->
-		<!-- <v-card>
+		<v-card>
 			<h2 align="center">¡Buscamos voluntarios!</h2>
 			<Voluntarios></Voluntarios>
 		</v-card> 
-		<hr>-->
+		<hr>
 
 		<section>
 			<h2 align="center">¿Qué ofrecemos?</h2>
