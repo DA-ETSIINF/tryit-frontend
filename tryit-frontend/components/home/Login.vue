6bbbc9559893ef207aadb0fbcc543491aa2150af
Christian <christian.most@alumnos.upm.es>
2026-03-09 21:57:58 +0100

    BUG FIX: fixed url for Non Upm attendees login where the url was hardcoded into 2025 instead of made dinamically using env.edition

--- tryit-frontend/components/home/Login.vue
+++ tryit-frontend/components/home/Login.vue
@@ -73,7 +73,7 @@ export default {
       try {
         const payload = { external_mail: this.externalMail };
         const res = await this.$axios.post(
-          "https://tryit.upm.es/api/editions/2025/send_verification_email/",
+          `ttps://tryit.upm.es/api/editions/${process.env.edition}/send_verification_email/`,
           payload
         );
         if (res.status === 200 || res.status === 201) {
